a3dcc1aba96f8a3eabdf6787bea5209ee85c6fcf
atakocabas <atakocabas@gmail.com>
2023-12-05 22:34:48 +0300

    initial

---
+++ src/main/java/com/catan/catanui/CatanUiApplication.java
@@ -0,0 +1,13 @@
+package com.catan.catanui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatanUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/catan/catanui/CatanUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catan.catanui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
