effaabcc579dce1cbffcb94f323f12526c57fd48
asrin <yilmaz.asrin@proton.me>
2023-12-06 18:49:35 +0300

    updated simple login page and addde "login successful" page

--- src/main/java/com/catan/catanui/controller/LoginController.java
+++ src/main/java/com/catan/catanui/controller/LoginController.java
@@ -1,9 +1,23 @@
 package com.catan.catanui.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.client.HttpClientErrorException;
 
+import java.io.IOException;
+
+@Controller
 public class LoginController {
 
     @FXML
@@ -12,13 +26,55 @@ public class LoginController {
     @FXML
     private PasswordField passwordField;
 
+    private String backendBaseUrl = "http://localhost:8080";
+
     @FXML
     private void handleLogin() {
-        // Logic to handle login
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // Here, add your logic to validate the username and password
-        System.out.println("Login attempted with Username: " + username + " and Password: [PROTECTED]");
+        RestTemplate restTemplate = new RestTemplate();
+
+        String apiUrl = backendBaseUrl + "/api/v1/user/login"; // Modify this URL according to your backend API
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("username", username)
+                .queryParam("password", password);
+
+        try {
+            ResponseEntity<HttpStatus> response = restTemplate.exchange(
+                    builder.toUriString(),
+                    HttpMethod.GET,
+                    null,
+                    HttpStatus.class
+            );
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                showSuccessPage();
+            } else {
+                System.err.println("Login unsuccessful");
+            }
+        } catch (HttpClientErrorException e) {
+            System.err.println("Login unsuccessful");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showSuccessPage() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login_successful.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Success!");
+            stage.setScene(new Scene(root, 300, 200));
+            stage.show();
+
+            Stage loginStage = (Stage) usernameField.getScene().getWindow();
+            loginStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
