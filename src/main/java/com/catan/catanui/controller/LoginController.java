c71d419d5f4e50ff4a1587371df2f9453c360f06
atakocabas <atakocabas@gmail.com>
2023-12-12 14:08:12 +0300

    Code Duplication Adjustment - Register Page Navigation

--- src/main/java/com/catan/catanui/controller/LoginController.java
+++ src/main/java/com/catan/catanui/controller/LoginController.java
@@ -1,80 +1,42 @@
 package com.catan.catanui.controller;
 
+import com.catan.catanui.constants.Constant;
+import com.catan.catanui.service.LoginService;
+import com.catan.catanui.utils.Navigate;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpMethod;
+import javafx.stage.Stage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.client.HttpClientErrorException;
 
-import java.io.IOException;
+import java.net.URL;
 
 @Controller
 public class LoginController {
-
     @FXML
     private TextField usernameField;
 
     @FXML
     private PasswordField passwordField;
 
-    private String backendBaseUrl = "http://localhost:8080";
+    private final LoginService loginService = new LoginService();
 
     @FXML
     private void handleLogin() {
         String username = usernameField.getText();
         String password = passwordField.getText();
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        String apiUrl = backendBaseUrl + "/api/v1/user/login"; // Modify this URL according to your backend API
-
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-                .queryParam("username", username)
-                .queryParam("password", password);
-
-        try {
-            ResponseEntity<HttpStatus> response = restTemplate.exchange(
-                    builder.toUriString(),
-                    HttpMethod.GET,
-                    null,
-                    HttpStatus.class
-            );
-
-            if (response.getStatusCode().is2xxSuccessful()) {
-                showSuccessPage();
-            } else {
-                System.err.println("Login unsuccessful");
-            }
-        } catch (HttpClientErrorException e) {
+        ResponseEntity<HttpStatus> response = loginService.login(username, password);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            URL mainUrl = getClass().getResource(Constant.MAIN);
+            Navigate.navigate((Stage) usernameField.getScene().getWindow(), mainUrl);
+        } else {
             System.err.println("Login unsuccessful");
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
-    private void showSuccessPage() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login_successful.fxml"));
-            Parent root = fxmlLoader.load();
-
-            Stage stage = new Stage();
-            stage.setTitle("Success!");
-            stage.setScene(new Scene(root, 300, 200));
-            stage.show();
-
-            Stage loginStage = (Stage) usernameField.getScene().getWindow();
-            loginStage.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void handleRegister() {
+        Navigate.navigate((Stage) usernameField.getScene().getWindow(), getClass().getResource(Constant.REGISTER));
     }
 }
