a447bb8f0add2f77b95b1df299f5dfc9b41c60f3
atakocabas <atakocabas@gmail.com>
2023-12-12 14:08:41 +0300

    Constants

---
+++ src/main/java/com/catan/catanui/constants/Constant.java
@@ -0,0 +1,9 @@
+package com.catan.catanui.constants;
+
+public class Constant {
+    public static final String FXML_PATH = "/fxml/";
+    public static final String LOGIN = FXML_PATH + "login.fxml";
+    public static final String MAIN = FXML_PATH + "main.fxml";
+    public static final String LEADERBOARD = FXML_PATH + "leaderboard.fxml";
+    public static final String REGISTER = FXML_PATH + "register.fxml";
+}
