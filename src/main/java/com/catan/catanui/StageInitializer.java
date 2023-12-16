50ccb5e66f9e3312c6bd4fbcdddbd1d34ca0eaa1
kaa-ceng <knkarakut@gmail.com>
2023-12-16 15:20:22 +0300

    size changed

--- src/main/java/com/catan/catanui/StageInitializer.java
+++ src/main/java/com/catan/catanui/StageInitializer.java
@@ -1,22 +1,16 @@
 package com.catan.catanui;
 
 import com.catan.catanui.constants.Constant;
-import javafx.geometry.Pos;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 
@@ -32,7 +26,7 @@ public class StageInitializer implements ApplicationListener<ClientApplication.S
             // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Constant.LOGIN));
             Parent root = fxmlLoader.load();
 
-            Scene scene = new Scene(root, 700, 700);
+            Scene scene = new Scene(root, 1100, 800);
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
