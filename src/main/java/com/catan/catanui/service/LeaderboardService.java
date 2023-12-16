d8322e16e389a3f8fef6bafaac69ea513071c9fc
asrin <yilmaz.asrin@proton.me>
2023-12-16 19:37:08 +0300

    Add authorization header to leaderboard API request

--- src/main/java/com/catan/catanui/service/LeaderboardService.java
+++ src/main/java/com/catan/catanui/service/LeaderboardService.java
@@ -1,6 +1,12 @@
 package com.catan.catanui.service;
 
+import com.catan.catanui.config.TokenStore;
 import com.catan.catanui.entity.LeaderboardEntry;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -27,7 +33,20 @@ public class LeaderboardService {
         List<LeaderboardEntry> leaderboardEntries = new ArrayList<>();
         try {
             RestTemplate restTemplate = new RestTemplate();
-            List<LinkedHashMap> entries = restTemplate.getForObject(apiUrl, List.class);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + TokenStore.getInstance().getToken());
+
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+            ResponseEntity<List<LinkedHashMap>> response = restTemplate.exchange(
+                    apiUrl,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<LinkedHashMap>>() {}
+            );
+
+            List<LinkedHashMap> entries = response.getBody();
 
             if(entries == null) throw new IllegalStateException("No entries found");
 
@@ -39,7 +58,7 @@ public class LeaderboardService {
                 leaderboardEntries.add(entry);
             }
         } catch (Exception e) {
-            System.out.println("An error occurred while fetching the weekly scores: " + e.getMessage());
+            System.out.println("An error occurred while fetching the leaderboard scores: " + e.getMessage());
         }
         return leaderboardEntries;
     }
