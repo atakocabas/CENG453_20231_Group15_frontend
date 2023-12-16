cfba86f43460cbf43ab920dd9c82a80133241025
kaa-ceng <knkarakut@gmail.com>
2023-12-16 16:06:42 +0300

    dice numbers randomly assigned

--- src/main/java/com/catan/catanui/controller/GameTablePackage/HexagonController.java
+++ src/main/java/com/catan/catanui/controller/GameTablePackage/HexagonController.java
@@ -5,6 +5,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.*;
@@ -17,27 +20,43 @@ public class HexagonController implements Initializable {
     @FXML
     private GridPane hexGridPane;
 
-    private static final List<Color> HEXAGON_COLORS = Arrays.asList(
+    private static final List<Color> HEXAGON_COLORS = new ArrayList<>(List.of(
             Color.DARKGREEN, Color.DARKGREEN, Color.DARKGREEN, Color.DARKGREEN, // 4 dark green hexagons
             Color.SADDLEBROWN, Color.SADDLEBROWN, Color.SADDLEBROWN, // 3 dark brown hexagons
             Color.LIGHTGREEN, Color.LIGHTGREEN, Color.LIGHTGREEN, Color.LIGHTGREEN, // 4 light green hexagons
             Color.GREY, Color.GREY, Color.GREY, // 3 light blue hexagons
             Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, // 4 yellow hexagons
             Color.BURLYWOOD // 1 sand-colored hexagon
-    );
+    ));
+
+    // Make sure the number of elements in HEXAGON_COLORS and HEXAGON_NUMBERS are the same
+    private static final List<Integer> HEXAGON_NUMBERS = new LinkedList<>(List.of(2, 3, 11, 12, 4, 5, 6, 7, 8, 9, 10, 4, 5, 6, 7, 8, 9, 10));
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // Shuffle the hexagon colors list to randomize the order
+        // Shuffle the hexagon colors and numbers lists to randomize the order
         Collections.shuffle(HEXAGON_COLORS);
+        Collections.shuffle(HEXAGON_NUMBERS);
 
         int row = 0;
         int column = 0;
 
-        // Iterate through the hexagons and assign colors
-        for (Color color : HEXAGON_COLORS) {
+        // Iterate through the hexagons and assign colors and numbers
+        for (int i = 0; i < HEXAGON_COLORS.size(); i++) {
+            Color color = HEXAGON_COLORS.get(i);
+            int number = (color.equals(Color.BURLYWOOD)) ? 0 : HEXAGON_NUMBERS.remove(0);
+
             Polygon hexagon = createHexagon(color);
             hexGridPane.add(hexagon, column, row);
+
+            // Add the number to the middle of the hexagon (skip if Burlywood)
+            if (!color.equals(Color.BURLYWOOD)) {
+                Text numberText = new Text(Integer.toString(number));
+                numberText.setFont(Font.font("Arial", FontWeight.BOLD, 16)); // Adjust font and thickness
+                hexGridPane.add(numberText, column, row);
+                GridPane.setHalignment(numberText, HPos.CENTER);
+                GridPane.setValignment(numberText, VPos.CENTER);
+            }
 
             // Increment row and column based on the grid pattern
             column++;
@@ -49,7 +68,6 @@ public class HexagonController implements Initializable {
         GridPane.setHalignment(hexGridPane, HPos.CENTER);
         GridPane.setValignment(hexGridPane, VPos.CENTER);
     }
-
 
     private Polygon createHexagon(Color fill) {
         Polygon hexagon = new Polygon();
