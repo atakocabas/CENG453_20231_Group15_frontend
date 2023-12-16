affb247db42f14c6db9dcb63b680f215d74b5954
asrin <yilmaz.asrin@proton.me>
2023-12-16 19:30:56 +0300

    Refactor Register in RegisterService to use Request Body

--- src/main/java/com/catan/catanui/service/RegisterService.java
+++ src/main/java/com/catan/catanui/service/RegisterService.java
@@ -1,25 +1,35 @@
 package com.catan.catanui.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterService {
     public ResponseEntity<HttpStatus> register(String username, String password, String email) {
         RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/api/v1/user/register")
-                .queryParam("username", username)
-                .queryParam("password", password)
-                .queryParam("email", email);
+        String url = "http://localhost:8080/api/v1/user/register";
+
+        Map<String, String> body = new HashMap<>();
+        body.put("username", username);
+        body.put("password", password);
+        body.put("email", email);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
 
         try {
             return restTemplate.exchange(
-                    builder.toUriString(),
+                    url,
                     HttpMethod.POST,
-                    null,
+                    entity,
                     HttpStatus.class
             );
         } catch (Exception e) {
