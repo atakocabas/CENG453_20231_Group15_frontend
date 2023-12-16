7bee8cd1a8b61ad4a71c53868e8b3b88bb9a4eb8
kaa-ceng <knkarakut@gmail.com>
2023-12-16 10:37:53 +0300

    dummy GameTableService

---
+++ src/main/java/com/catan/catanui/service/GameTableService.java
@@ -0,0 +1,27 @@
+package com.catan.catanui.service;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class GameTableService {
+    public ResponseEntity<HttpStatus> login(String username, String password){
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/api/v1/user/login")
+                .queryParam("username", username)
+                .queryParam("password", password);
+        try{
+            return restTemplate.exchange(
+                    builder.toUriString(),
+                    HttpMethod.GET,
+                    null,
+                    HttpStatus.class
+            );
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
