0251a6b90cc1192c0658d06985f4b73bf7949a28
kaa-ceng <knkarakut@gmail.com>
2023-12-17 20:19:26 +0300

    empty

--- src/main/java/com/catan/catanui/service/GameTableService.java
+++ src/main/java/com/catan/catanui/service/GameTableService.java
@@ -7,7 +7,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class GameTableService {
-    public ResponseEntity<HttpStatus> login(String username, String password){
+
+    //EMPTY NOW
+
+    /*public ResponseEntity<HttpStatus> login(String username, String password){
         RestTemplate restTemplate = new RestTemplate();
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/api/v1/user/login")
                 .queryParam("username", username)
@@ -23,5 +26,5 @@ public class GameTableService {
             e.printStackTrace();
         }
         return null;
-    }
+    }*/
 }
