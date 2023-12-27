9bc48c0fc1ab60bf474328436188293dfc5dc81d
atakocabas <atakocabas@gmail.com>
2023-12-27 16:04:00 +0300

    Current Turn Indicator

--- src/main/java/com/catan/catanui/controller/game/PlayerController.java
+++ src/main/java/com/catan/catanui/controller/game/PlayerController.java
@@ -3,6 +3,7 @@ package com.catan.catanui.controller.game;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
@@ -18,7 +19,7 @@ public class PlayerController implements Initializable {
 
     int fontSize = 20;
 
-    String textColor =  "white";
+    String textColor = "white";
     @FXML
     private VBox playerVBox;
 
@@ -46,10 +47,30 @@ public class PlayerController implements Initializable {
     @FXML
     private Text player4Text;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // You can initialize player-related components here if needed
+        instance = this;
+        updatePlayerCircle(0);
+    }
+
+    public static PlayerController getInstance() {
+        if (instance == null) {
+            instance = new PlayerController();
+        }
+        return instance;
+    }
+
+    public void updatePlayerCircle(int currentPlayer) {
+        Circle[] playerCircles = { player1Circle, player2Circle, player3Circle, player4Circle };
+        for (int i = 0; i < playerCircles.length; i++) {
+            if (i == currentPlayer) {
+                playerCircles[i].setStroke(Color.GREEN);
+                playerCircles[i].setStrokeWidth(5.0);
+            } else {
+                playerCircles[i].setStroke(Color.BLACK);
+                playerCircles[i].setStrokeWidth(1.0);
+            }
+        }
     }
 }
-
