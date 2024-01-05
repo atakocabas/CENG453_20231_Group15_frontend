f0aceb653cde7beee3e35a4541727633515ecbc0
atakocabas <atakocabas@gmail.com>
2024-01-05 17:35:43 +0300

    Disable adjacent settlement buttons when a settlement is built

--- src/main/java/com/catan/catanui/entity/SettlementButton.java
+++ src/main/java/com/catan/catanui/entity/SettlementButton.java
@@ -45,6 +45,7 @@ public class SettlementButton extends Circle implements EventHandler<MouseEvent>
             this.setOwner(currentPlayer);
             this.setFill(currentPlayer.getColor());
             this.setDisable(true);
+            this.getAdjacentSettlementButtons().forEach(settlementButton -> settlementButton.setDisable(true));
             logger.info("Settlement Built by Player: {}", currentPlayer.getId());
         } else {
             logger.info("Not Enough Resources to Build Settlement.");
