e26106eba98b38838db07bca36202a554f33411a
atakocabas <atakocabas@gmail.com>
2024-01-07 15:56:17 +0300

    ButtonsController JavaDoc

--- src/main/java/com/catan/catanui/controller/ButtonsController.java
+++ src/main/java/com/catan/catanui/controller/ButtonsController.java
@@ -7,9 +7,16 @@ import com.catan.catanui.entity.Player;
 import com.catan.catanui.entity.RoadButton;
 import com.catan.catanui.entity.SettlementButton;
 
+/**
+ * The ButtonsController class is responsible for managing the enable/disable state of various buttons in the UI.
+ */
 public class ButtonsController {
     private static ButtonsController instance;
 
+    /**
+     * The ButtonsController class is responsible for managing the buttons in the user interface.
+     * It follows the Singleton design pattern to ensure that only one instance of the class is created.
+     */
     public static ButtonsController getInstance() {
         if (instance == null) {
             instance = new ButtonsController();
@@ -17,6 +24,11 @@ public class ButtonsController {
         return instance;
     }
 
+    /**
+     * Enables the available road buttons for a given player.
+     * 
+     * @param player the player for whom to enable the road buttons
+     */
     public void enableAvailableRoadButtons(Player player) {
         List<RoadButton> availableRoadButtons = player.getAvaliableRoadButtons();
         for (RoadButton roadButton : availableRoadButtons) {
@@ -25,6 +37,9 @@ public class ButtonsController {
     }
 
 
+    /**
+     * Disables all road buttons in the game.
+     */
     public void disableAllRoadButtons() {
         List<RoadButton> roadButtons = GameTableController.getInstance().getRoadButtons();
         for (RoadButton roadButton : roadButtons) {
@@ -32,6 +47,9 @@ public class ButtonsController {
         }
     }
 
+    /**
+     * Disables all settlement buttons.
+     */
     public void disableAllSettlementButtons(){
         List<SettlementButton> settlementButtons = GameTableController.getInstance().getSettlementButtons();
         for (SettlementButton settlementButton : settlementButtons) {
@@ -39,6 +57,11 @@ public class ButtonsController {
         }
     }
 
+    /**
+     * Enables the available settlement buttons for a given player.
+     * 
+     * @param player the player for whom to enable the settlement buttons
+     */
     public void enableAvailableSettlementButtons(Player player) {
         List<SettlementButton> availableSettlementButtons = player.getAvaliableSettlementButtons();
         for (SettlementButton settlementButton : availableSettlementButtons) {
@@ -46,6 +69,11 @@ public class ButtonsController {
         }
     }
 
+    /**
+     * Enables the upgradable settlement buttons for the specified player.
+     * 
+     * @param player the player for whom to enable the buttons
+     */
     public void enableUpgradableSettlementButtons(Player player){
         List<SettlementButton> availableSettlementButtons = player.getUpgradableSettlementButtons();
         for (SettlementButton settlementButton : availableSettlementButtons) {
@@ -54,11 +82,18 @@ public class ButtonsController {
     }
 
 
+    /**
+     * Enables all game table buttons.
+     * This method enables all settlement buttons and road buttons on the game table.
+     */
     public void enableAllGameTableButtons() {
         enableAllSettlementButtons();
         enableAllRoadButtons();
     }
 
+    /**
+     * Enables all settlement buttons.
+     */
     private void enableAllSettlementButtons() {
         List<SettlementButton> settlementButtons = GameTableController.getInstance().getSettlementButtons();
         for (SettlementButton settlementButton : settlementButtons) {
@@ -66,6 +101,9 @@ public class ButtonsController {
         }
     }
 
+    /**
+     * Enables all road buttons in the game.
+     */
     private void enableAllRoadButtons(){
         List<RoadButton> roadButtons = GameTableController.getInstance().getRoadButtons();
         for (RoadButton roadButton : roadButtons) {
